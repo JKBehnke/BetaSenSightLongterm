9b497e7050fcdcebeb6382455ec68cacf3a58ed8
JKBehnke <114879131+JKBehnke@users.noreply.github.com>
2023-05-15 16:09:51 +0200

    cluster permutation only of highest beta channel

--- src/bssu/bipolar/cluster_perm.py
+++ src/bssu/bipolar/cluster_perm.py
@@ -123,7 +123,11 @@ def cluster_permutation_power_spectra_betw_sessions(
           "\nwritten in: ", results_path
           )
 
-    return results_df
+    return {
+        "results_df": results_df,
+        "x_session_1": x_session_1
+
+    }
         
         
 
@@ -285,9 +289,274 @@ def cluster_permutation_fooof_power_spectra(
         "comparisons_storage": comparisons_storage
           }
             
+
+def highest_beta_channels_fooof(
+        fooof_spectrum:str
+):
+    """
+    Load the file "fooof_model_group_data.json"
+    from the group result folder
+
+    Input: 
+        - fooof_spectrum: 
+            "periodic_spectrum"         -> 10**(model._peak_fit + model._ap_fit) - (10**model._ap_fit)
+            "periodic_plus_aperiodic"   -> model._peak_fit + model._ap_fit (log(Power))
+            "periodic_flat"             -> model._peak_fit
+
+    1) calculate beta average for each channel and rank within 1 stn, 1 session and 1 channel group
+    
+    2) rank beta averages and only select the channels with rank 1.0 
+
+    Output highest_beta_df
+        - containing all stns, all sessions, all channels with rank 1.0 within their channel group
+    
+    """
+
+    # load the group dataframe
+    fooof_group_result = loadResults.load_group_fooof_result()
+
+    # create new column: first duplicate column fooof power spectrum, then apply calculation to each row -> average of indices [13:36] so averaging the beta range
+    fooof_group_result_copy = fooof_group_result.copy()
+
+    if fooof_spectrum == "periodic_spectrum":
+        fooof_group_result_copy["beta_average"] = fooof_group_result_copy["fooof_power_spectrum"]
+    
+    elif fooof_spectrum == "periodic_plus_aperiodic":
+        fooof_group_result_copy["beta_average"] = fooof_group_result_copy["periodic_plus_aperiodic_power_log"]
+
+    elif fooof_spectrum == "periodic_flat":
+        fooof_group_result_copy["beta_average"] = fooof_group_result_copy["fooof_periodic_flat"]
+    
+    
+    fooof_group_result_copy["beta_average"] = fooof_group_result_copy["beta_average"].apply(lambda row: np.mean(row[13:36]))
+
+
+    ################################ WRITE DATAFRAME ONLY WITH HIGHEST BETA CHANNELS PER STN | SESSION | CHANNEL_GROUP ################################
+    channel_group = ["ring", "segm_inter", "segm_intra"]
+    sessions = ["postop", "fu3m", "fu12m", "fu18m"]
+
+    stn_unique = fooof_group_result_copy.subject_hemisphere.unique().tolist()
+
+    highest_beta_df = pd.DataFrame()
+
+    for stn in stn_unique:
+
+        stn_df = fooof_group_result_copy.loc[fooof_group_result_copy.subject_hemisphere == stn]
+
+        for ses in sessions:
+
+            # check if session exists
+            if ses not in stn_df.session.values:
+                continue
+
+            else:
+                stn_ses_df = stn_df.loc[stn_df.session == ses] # df of only 1 stn and 1 session
+
+            for group in channel_group:
+
+                if group == "ring":
+                    channels = ['01', '12', '23']
+                    
+                elif group == "segm_inter":
+                    channels = ["1A2A", "1B2B", "1C2C"]
+                
+                elif group == "segm_intra":
+                    channels = ['1A1B', '1B1C', '1A1C', '2A2B', '2B2C', '2A2C']
+
+                group_comp_df = stn_ses_df.loc[stn_ses_df["bipolar_channel"].isin(channels)].reset_index() # df of only 1 stn, 1 session and 1 channel group
+
+                # rank beta average of channels within one channel group
+                group_comp_df_copy = group_comp_df.copy()
+                group_comp_df_copy["beta_rank"] = group_comp_df_copy["beta_average"].rank(ascending=False) 
+
+                # only keep the row with beta rank 1.0
+                group_comp_df_copy = group_comp_df_copy.loc[group_comp_df_copy.beta_rank == 1.0]
+
+                # save to ranked_beta_df
+                highest_beta_df = pd.concat([highest_beta_df, group_comp_df_copy])
+
+    return highest_beta_df
             
             
             
+def cluster_permutation_fooof_power_spectra_highest_beta(
+        fooof_spectrum:str,
+        min_freq:int,
+        max_freq:int
+):
+    """
+    Load the modified FOOOF dataframe with this function: highest_beta_channels_fooof()
+    calculates beta average for each channel and rank within 1 stn, 1 session and 1 channel group
+    and only selects the channels with rank 1.0 
+
+    Input: 
+        - fooof_spectrum: 
+            "periodic_spectrum"         -> 10**(model._peak_fit + model._ap_fit) - (10**model._ap_fit)
+            "periodic_plus_aperiodic"   -> model._peak_fit + model._ap_fit (log(Power))
+            "periodic_flat"             -> model._peak_fit
+
+        cluster permutation only within a frequency band from min_freq to max_freq
+        - min_freq: e.g. 5 Hz 
+        - max_freq: e.g. 35 Hz 
+  
+    1) Get the Dataframes for each session comparison and for channel groups seperately
+        - within each session comparison -> only STNs are included, that have recordings at both sessions (same sample size per comparison)
+    
+    2) for each comparison make a list with two arrays, one per session
+        - use np.vstack to create one big array with power spectra vectors per session
+        - only include power up until maximal frequency (95 Hz) by indexing
+        - shape of one array/one session = (n_observations, )
+    
+    3) perform cluster permutation per session comparison:
+        - comparisons: ["postop_fu3m", "postop_fu12m", "postop_fu18m", 
+                        "fu3m_fu12m", "fu3m_fu18m", "fu12m_fu18m"]
+        - number of Permutations = 1000
+        
+    Output of mne.stats.permutation_cluster_test:
+        - F_obs, shape (p[, q][, r]): 
+            Statistic (F by default) observed for all variables.
+
+        - clusterslist: 
+            List type defined by out_type above.
+
+        - cluster_pvarray
+            P-value for each cluster.
+
+        - H0array, shape (n_permutations,)
+            Max cluster level stats observed under permutation.
+    
+    """
+    
+    results_path = find_folders.get_local_path(folder="GroupResults")
+
+
+    ################################ WRITE DATAFRAME ONLY WITH HIGHEST BETA CHANNELS PER STN | SESSION | CHANNEL_GROUP ################################
+    fooof_group_result = highest_beta_channels_fooof(fooof_spectrum=fooof_spectrum)
+
+    # containing all stns, all sessions, all channels rank 1.0 within their channel group
+
+
+    ################################ Dataframes for each session comparison ################################
+    compare_sessions = ["postop_fu3m", "postop_fu12m", "postop_fu18m", 
+                        "fu3m_fu12m", "fu3m_fu18m", "fu12m_fu18m"]
+    
+    channel_group = ["ring", "segm_inter", "segm_intra"]
+
+    comparisons_storage = {}
+
+    for comparison in compare_sessions:
+
+        two_sessions = comparison.split("_")
+        session_1 = two_sessions[0]
+        session_2 = two_sessions[1]
+
+        # Dataframe per session
+        session_1_df = fooof_group_result.loc[(fooof_group_result["session"]==session_1)]
+        session_2_df = fooof_group_result.loc[(fooof_group_result["session"]==session_2)]
+
+        # list of STNs per session
+        session_1_stns = list(session_1_df.subject_hemisphere.unique())
+        session_2_stns = list(session_2_df.subject_hemisphere.unique())
+
+        # list of STNs included in both sessions
+        STN_list = list(set(session_1_stns) & set(session_2_stns))
+        STN_list.sort()
+
+        comparison_df = pd.DataFrame()
+
+        for stn in STN_list:
+
+            session_1_compared_to_2 = session_1_df.loc[session_1_df["subject_hemisphere"]==stn]
+            session_2_compared_to_1 = session_2_df.loc[session_2_df["subject_hemisphere"]==stn]
             
+            comparison_df = pd.concat([comparison_df, session_1_compared_to_2, session_2_compared_to_1])
             
+
+        comparisons_storage[f"{comparison}"] = comparison_df
+    
+
+    ################################ CLUSTER PERMUTATION FOR EACH SESSION COMPARISON; SEPERATELY IN CHANNEL GROUPS ###############################
+    
+    permutation_results = {}
+
+    # filter each comparison Dataframe for comparisons and channels in each channel group
+    for comp in compare_sessions:
+
+        comparison_df = comparisons_storage[f"{comp}"]
+
+        for group in channel_group:
+
+            if group == "ring":
+                channels = ['01', '12', '23']
+            
+            elif group == "segm_inter":
+                channels = ["1A2A", "1B2B", "1C2C"]
+            
+            elif group == "segm_intra":
+                channels = ['1A1B', '1B1C', '1A1C', '2A2B', '2B2C', '2A2C']
+
+            # dataframe only of one comparison and one channel group
+            group_comp_df = comparison_df.loc[comparison_df["bipolar_channel"].isin(channels)]
+
+            two_sessions = comp.split("_")
+            session_1 = two_sessions[0] # e.g. postop
+            session_2 = two_sessions[1]
+
+            # only get part of df with session_1 and df with session_2
+            session_1_df = group_comp_df.loc[group_comp_df.session==session_1]
+            session_2_df = group_comp_df.loc[group_comp_df.session==session_2]
+
+            if fooof_spectrum == "periodic_spectrum":
+                power_column = "fooof_power_spectrum"
+        
+            elif fooof_spectrum == "periodic_plus_aperiodic":
+                power_column = "periodic_plus_aperiodic_power_log"
+
+            elif fooof_spectrum == "periodic_flat":
+                power_column = "fooof_periodic_flat"
+
+            # from each session df only take the values from column with power spectra only until maximal frequency 95
+            x_session_1 = np.vstack(session_1_df[power_column].values)[:, min_freq:max_freq+1]
+            # e.g. for shape x_18mfu comparison to 12mfu = (10, 90), 10 STNs, 90 values per STN
+            x_session_2 = np.vstack(session_2_df[power_column].values)[:,min_freq:max_freq+1]
+
+            list_for_cluster_permutation = [x_session_1, x_session_2]
+
+            # perform cluster permutation
+            F_obs, clusters, cluster_pv, H0 = permutation_cluster_test(list_for_cluster_permutation, n_permutations=1000)
+
+            # get the sample size
+            sample_size = len(session_1_df.fooof_power_spectrum.values)
+
+            # save results
+            permutation_results[f"{comp}_{group}"] = [comp, group, F_obs, clusters, cluster_pv, H0, sample_size]
+    
+
+    results_df = pd.DataFrame(permutation_results)
+    results_df.rename(index={
+        0: "session_comparison",
+        1: "channel_group",
+        2: "F_obs",
+        3: "clusters",
+        4: "cluster_pv",
+        5: "H0",
+        6: "sample_size"
+    }, inplace=True)
+    results_df = results_df.transpose()
+
+    # save the results DF into results path
+    results_df_filepath = os.path.join(results_path, f"cluster_permutation_fooof_highest_beta_spectra_session_comparisons.pickle")
+    with open(results_df_filepath, "wb") as file:
+        pickle.dump(results_df, file)
+    
+    print("file: ", 
+          f"cluster_permutation_fooof_highest_beta_spectra_session_comparisons.pickle",
+          "\nwritten in: ", results_path
+          )
+
+    return {
+        "results_df": results_df,
+        "comparisons_storage": comparisons_storage,
+        "fooof_group_result": fooof_group_result
+          }
             
