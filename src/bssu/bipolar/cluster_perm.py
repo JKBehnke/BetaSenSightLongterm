e4557e6b05eecf9dc26cf53a1aafee9ad788414d
JKBehnke <jennifer-kim.behnke@t-online.de>
2023-05-20 19:10:08 +0200

    cluster permutation for highest postop channel

--- src/bssu/bipolar/cluster_perm.py
+++ src/bssu/bipolar/cluster_perm.py
@@ -291,7 +291,8 @@ def cluster_permutation_fooof_power_spectra(
             
 
 def highest_beta_channels_fooof(
-        fooof_spectrum:str
+        fooof_spectrum:str,
+        highest_beta_session:str
 ):
     """
     Load the file "fooof_model_group_data.json"
@@ -302,6 +303,8 @@ def highest_beta_channels_fooof(
             "periodic_spectrum"         -> 10**(model._peak_fit + model._ap_fit) - (10**model._ap_fit)
             "periodic_plus_aperiodic"   -> model._peak_fit + model._ap_fit (log(Power))
             "periodic_flat"             -> model._peak_fit
+
+        - highest_beta_session: "highest_postop", "highest_fu3m", "highest_each_session"
 
     1) calculate beta average for each channel and rank within 1 stn, 1 session and 1 channel group
     
@@ -337,7 +340,7 @@ def highest_beta_channels_fooof(
 
     stn_unique = fooof_group_result_copy.subject_hemisphere.unique().tolist()
 
-    highest_beta_df = pd.DataFrame()
+    beta_rank_df = pd.DataFrame()
 
     for stn in stn_unique:
 
@@ -351,6 +354,7 @@ def highest_beta_channels_fooof(
 
             else:
                 stn_ses_df = stn_df.loc[stn_df.session == ses] # df of only 1 stn and 1 session
+
 
             for group in channel_group:
 
@@ -369,18 +373,124 @@ def highest_beta_channels_fooof(
                 group_comp_df_copy = group_comp_df.copy()
                 group_comp_df_copy["beta_rank"] = group_comp_df_copy["beta_average"].rank(ascending=False) 
 
-                # only keep the row with beta rank 1.0
-                group_comp_df_copy = group_comp_df_copy.loc[group_comp_df_copy.beta_rank == 1.0]
-
                 # save to ranked_beta_df
-                highest_beta_df = pd.concat([highest_beta_df, group_comp_df_copy])
+                beta_rank_df = pd.concat([beta_rank_df, group_comp_df_copy])
+    
+    # depending on input: keep only rank 1.0 or keep postop rank 1 or 3MFU rank 1 channel
+    if highest_beta_session == "highest_each_session":
+        # only keep the row with beta rank 1.0
+        highest_beta_df = beta_rank_df.loc[beta_rank_df.beta_rank == 1.0]
+    
+    elif highest_beta_session == "highest_postop":
+        highest_beta_df = pd.DataFrame()
+        # for each stn get channel name of beta rank 1 in postop and select the channels for the other timepoints
+        for stn in stn_unique:
+
+            stn_data = beta_rank_df.loc[beta_rank_df.subject_hemisphere == stn]
+            
+            for ses in sessions:
+                # check if postop exists
+                if "postop" not in stn_data.session.values:
+                    continue
+
+                elif ses not in stn_data.session.values:
+                    continue
+                
+                else: 
+                    postop_rank1_channels = stn_data.loc[stn_data.session=="postop"]
+                    postop_rank1_channels = postop_rank1_channels.loc[postop_rank1_channels.beta_rank == 1.0]
+
+                    stn_ses_data = stn_data.loc[stn_data.session == ses]
+                
+                for group in channel_group:
+
+                    if group == "ring":
+                        channels = ['01', '12', '23']
+                    
+                    elif group == "segm_inter":
+                        channels = ["1A2A", "1B2B", "1C2C"]
+                    
+                    elif group == "segm_intra":
+                        channels = ['1A1B', '1B1C', '1A1C', '2A2B', '2B2C', '2A2C']
+
+                    group_data = stn_ses_data.loc[stn_ses_data["bipolar_channel"].isin(channels)].reset_index()
+
+                    # get channel name of rank 1 channel in postop in this channel group
+                    postop_1_row = postop_rank1_channels.loc[postop_rank1_channels["bipolar_channel"].isin(channels)]
+                    postop_1_channelname = postop_1_row.bipolar_channel.values[0]
+
+                    # select only this channel in all the other sessions
+                    selected_rows = group_data.loc[group_data.bipolar_channel == postop_1_channelname]
+                    highest_beta_df = pd.concat([highest_beta_df, postop_1_row, selected_rows])
+        
+        # drop index columns 
+        # drop duplicated postop rows
+        # highest_beta_df = highest_beta_df.drop(column=["level_0", "index"])
+        highest_beta_df = highest_beta_df.drop_duplicates(keep="first", subset=["subject_hemisphere", "session", "bipolar_channel"])
+
+
+    elif highest_beta_session == "highest_fu3m":
+        highest_beta_df = pd.DataFrame()
+        # for each stn get channel name of beta rank 1 in postop and select the channels for the other timepoints
+        for stn in stn_unique:
+
+            stn_data = beta_rank_df.loc[beta_rank_df.subject_hemisphere == stn]
+            
+            for ses in sessions:
+
+                # if session is postop, continue, because we´re only interested in follow ups here
+                if ses == "postop":
+                    continue
+
+                # check if fu3m exists
+                if "fu3m" not in stn_data.session.values:
+                    continue
+
+                elif ses not in stn_data.session.values:
+                    continue
+                
+                else: 
+                    fu3m_rank1_channels = stn_data.loc[stn_data.session=="fu3m"]
+                    fu3m_rank1_channels = fu3m_rank1_channels.loc[fu3m_rank1_channels.beta_rank == 1.0]
+
+                    stn_ses_data = stn_data.loc[stn_data.session == ses]
+                
+                for group in channel_group:
+
+                    if group == "ring":
+                        channels = ['01', '12', '23']
+                    
+                    elif group == "segm_inter":
+                        channels = ["1A2A", "1B2B", "1C2C"]
+                    
+                    elif group == "segm_intra":
+                        channels = ['1A1B', '1B1C', '1A1C', '2A2B', '2B2C', '2A2C']
+
+                    group_data = stn_ses_data.loc[stn_ses_data["bipolar_channel"].isin(channels)].reset_index()
+
+                    # get channel name of rank 1 channel in fu3m in this channel group
+                    fu3m_1_row = fu3m_rank1_channels.loc[fu3m_rank1_channels["bipolar_channel"].isin(channels)]
+                    fu3m_1_channelname = fu3m_1_row.bipolar_channel.values[0]
+
+                    # select only this channel in all the other sessions
+                    selected_rows = group_data.loc[group_data.bipolar_channel == fu3m_1_channelname]
+                    highest_beta_df = pd.concat([highest_beta_df, fu3m_1_row, selected_rows])
+        
+        # drop index columns 
+        # drop duplicated postop rows
+        # highest_beta_df = highest_beta_df.drop(column=["level_0", "index"])
+        highest_beta_df = highest_beta_df.drop_duplicates(keep="first", subset=["subject_hemisphere", "session", "bipolar_channel"])
+
+
 
     return highest_beta_df
+        
             
             
             
 def cluster_permutation_fooof_power_spectra_highest_beta(
         fooof_spectrum:str,
+        highest_beta_session:str,
         min_freq:int,
         max_freq:int
 ):
@@ -394,6 +504,8 @@ def cluster_permutation_fooof_power_spectra_highest_beta(
             "periodic_spectrum"         -> 10**(model._peak_fit + model._ap_fit) - (10**model._ap_fit)
             "periodic_plus_aperiodic"   -> model._peak_fit + model._ap_fit (log(Power))
             "periodic_flat"             -> model._peak_fit
+        
+        - highest_beta_session: "highest_postop", "highest_fu3m", "highest_each_session"
 
         cluster permutation only within a frequency band from min_freq to max_freq
         - min_freq: e.g. 5 Hz 
@@ -431,14 +543,23 @@ def cluster_permutation_fooof_power_spectra_highest_beta(
 
 
     ################################ WRITE DATAFRAME ONLY WITH HIGHEST BETA CHANNELS PER STN | SESSION | CHANNEL_GROUP ################################
-    fooof_group_result = highest_beta_channels_fooof(fooof_spectrum=fooof_spectrum)
+    fooof_group_result = highest_beta_channels_fooof(fooof_spectrum=fooof_spectrum,
+                                                     highest_beta_session=highest_beta_session)
 
     # containing all stns, all sessions, all channels rank 1.0 within their channel group
 
 
     ################################ Dataframes for each session comparison ################################
-    compare_sessions = ["postop_fu3m", "postop_fu12m", "postop_fu18m", 
-                        "fu3m_fu12m", "fu3m_fu18m", "fu12m_fu18m"]
+    if highest_beta_session == "highest_each_session":
+        compare_sessions = ["postop_fu3m", "postop_fu12m", "postop_fu18m", 
+                            "fu3m_fu12m", "fu3m_fu18m", "fu12m_fu18m"]
+        
+    elif highest_beta_session == "highest_postop":
+        compare_sessions = ["postop_fu3m", "postop_fu12m", "postop_fu18m", 
+                            "fu3m_fu12m", "fu3m_fu18m", "fu12m_fu18m"]
+        
+    elif highest_beta_session == "highest_fu3m":
+        compare_sessions = ["fu3m_fu12m", "fu3m_fu18m", "fu12m_fu18m"]
     
     channel_group = ["ring", "segm_inter", "segm_intra"]
 
@@ -545,12 +666,12 @@ def cluster_permutation_fooof_power_spectra_highest_beta(
     results_df = results_df.transpose()
 
     # save the results DF into results path
-    results_df_filepath = os.path.join(results_path, f"cluster_permutation_fooof_highest_beta_{min_freq}_{max_freq}Hz_spectra_session_comparisons.pickle")
+    results_df_filepath = os.path.join(results_path, f"cluster_permutation_fooof_{highest_beta_session}_beta_{min_freq}_{max_freq}Hz_spectra_session_comparisons.pickle")
     with open(results_df_filepath, "wb") as file:
         pickle.dump(results_df, file)
     
     print("file: ", 
-          f"cluster_permutation_fooof_highest_beta_{min_freq}_{max_freq}Hz_spectra_session_comparisons.pickle",
+          f"cluster_permutation_fooof_{highest_beta_session}_beta_{min_freq}_{max_freq}Hz_spectra_session_comparisons.pickle",
           "\nwritten in: ", results_path
           )
 
@@ -560,3 +681,4 @@ def cluster_permutation_fooof_power_spectra_highest_beta(
         "fooof_group_result": fooof_group_result
           }
             
+
